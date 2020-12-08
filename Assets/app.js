f144d199b4e6f046a6c99e902ef29f6e524171ab
Francisco Rones <ininorones@gmail.com>
2020-12-07 22:09:47 -0800

    Addition: Repo1 appended to the main HTML

--- Assets/app.js
+++ Assets/app.js
@@ -1,30 +1,27 @@
+let repo1EL = document.querySelector(".repo1");
 
-
-let getMyRepo = function () {
+function getMyRepo(){
     let apiUrl = 'https://api.github.com/users/francisN21/repos';
   
     fetch(apiUrl)
       .then( (response) => {
           console.log(response);
-          let data = response.json()
+          let data = response.json();
           return data;
       })
       .then( (data) => {
             console.log(data);
-
-            console.log(data[10].full_name);
-            console.log(data[10].html_url);
-            console.log(data[10].description);
-            console.log(data[10].open_issue);
-            console.log(data[10].languages);
+            // r1 for repo 1
+            data.nameR1 = data[10].full_name;
+            repo1EL.innerHTML = `${data.nameR1}`;
+            data.htmlR1 = data[10].html_url;
+            data.descriptionR1 = data[10].description;
+            data.issueR1 = data[10].open_issue;
+            data.langR1 = data[10].languages;
 
             console.log(data[8]);
             console.log(data[4]);
       })
-      .then( () => {
-        
-      })
-        
   };
 
 getMyRepo();
